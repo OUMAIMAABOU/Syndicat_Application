5f9329db12bb6afaca5d2d869d200abea66b8b31
oumaima <oumaimaabouelhaitam@gmail.com>
2023-01-03 10:15:17 +0100

    create function DeleteClient

--- Backend/Controllers/ClientControllers.js
+++ Backend/Controllers/ClientControllers.js
@@ -1,26 +1,33 @@
-const Client =require('../Models/ClientModel')
+const Client = require("../Models/ClientModel");
 
 exports.CreateClient = async (req, res) => {
+  try {
+    const { body } = req;
+    if (await Client.findOne({ CIN: req.body.CIN }))
+      res.status(400).send("already existe");
+    else {
+      if (await Client.create({ ...body })) res.status(200).send("create");
+      else res.status(400).json("some thing worning");
+    }
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+};
+
+exports.UpdateClient = async (req, res) => {
+  try {
+    const { body } = req;
+    if (await Client.updateOne({ _id: req.params.id }, { ...body }))
+      res.status(200).send("update");
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+exports.DeleteClient = async (req, res) => {
     try {
-      const { body } = req;
-      if (await Client.findOne({ CIN: req.body.CIN}))
-        res.status(400).send("already existe");
-      else {
-        if (await Client.create({ ...body })) res.status(200).send("create");
-        else res.status(400).json("some thing worning");
-      }
+      if(await Client.deleteOne({_id:req.params.id}))  res.status(200).send("delete");
     } catch (e) {
-      res.status(400).send(e.message);
+      res.status(400).send(e);
     }
   };
-
-  exports.UpdateClient=async(req,res)=>{
-    try{
-  const { body}=req;
-  if(await Client.updateOne({_id:req.params.id},{...body})) res.status(200).send("update")
-  
-    }catch(e){
-      res.status(400).send(e);
-  
-    }
-  }
--- Backend/Routers/ClientRouter.js
+++ Backend/Routers/ClientRouter.js
@@ -3,5 +3,6 @@ const Client = require("../Controllers/ClientControllers");
 module.exports = app => {
   router.post("/create_client", Client.CreateClient);
   router.put("/update_client/:id", Client.UpdateClient);
+  router.delete("/delete_client/:id", Client.DeleteClient);
   app.use("/api/auth/", router);
 };
