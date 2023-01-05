f56ec713b6a8e109a054f183efeb8ad2a48c283d
oumaima <oumaimaabouelhaitam@gmail.com>
2023-01-05 23:59:30 +0100

    consommé login

--- frontend/src/Component/Login/Form.jsx
+++ frontend/src/Component/Login/Form.jsx
@@ -1,36 +1,55 @@
-import { useState } from "react";
+import {POST} from '../../Api/Axios'
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
 export default function Form() {
+  const [Data, SetData] = useState({});
+  const navig = useNavigate();
+
+  const handleChange = (e) => {
+    SetData({
+      ...Data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    try {
+      e.preventDefault();
+      POST("auth/login", Data).then((res) => {
+        if(res.data) {
+          localStorage.setItem("token", response.data.token);
+          navig("/appartement");
+      }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h3>Sign In</h3>
       <div className="mb-3">
         <label>Email address</label>
         <input
           type="email"
+          name='email'
           className="form-control"
           placeholder="Enter email"
+          onChange={handleChange}
         />
       </div>
       <div className="mb-3">
         <label>Password</label>
         <input
           type="password"
+          name='password'
           className="form-control"
           placeholder="Enter password"
+          onChange={handleChange}
         />
       </div>
-      <div className="mb-3">
-        <div className="custom-control custom-checkbox">
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="customCheck1"
-          />
-          <label className="custom-control-label" htmlFor="customCheck1">
-            Remember me
-          </label>
-        </div>
-      </div>
+     
       <div className="d-grid">
         <button type="submit" className="btn btn-primary">
           Submit
